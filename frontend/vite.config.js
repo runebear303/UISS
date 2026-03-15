a1d3760f3851e15f8a184de035d27ab03b460e55
runebear303 <exhileboy_r@hotmail.com>
2026-03-15 06:33:50 -0300

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,         // Dit zorgt ervoor dat Vite luistert op alle netwerkinterfaces (0.0.0.0)
+    port: 3000,         // Zorg dat dit overeenkomt met je Dockerfile en docker-compose
+    watch: {
+      usePolling: true, // Cruciaal voor Windows: zorgt dat wijzigingen in je code direct verwerkt worden
+    },
+  },
 })
